e4fed181dccf0fe4258cb573f4b7551c53d56509
unknown <8937055@6-306-10.grad.usricmc.icmc.usp.br>
2015-06-23 13:35:27 -0300

    projeto add no git

---
+++ OrgParte2.v2.2/Processador_Template_FPGA_DE115/AP9 - Template DE115/AP9.cdf
@@ -0,0 +1,13 @@
+/* Quartus II 64-Bit Version 13.0.1 Build 232 06/12/2013 Service Pack 1 SJ Web Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP4CE115F29) Path("C:/Users/8937055/Desktop/OrgParte2.v2.1/Processador_Template_FPGA_DE115/AP9 - Template DE115/") File("AP9.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
---
+++ OrgParte2.v2.2/Simulador_Windows_Tudo_Pronto_Fácil/Simulador/teste.asm
@@ -0,0 +1,80 @@
+main:
+	loadn r0, #' '
+	loadn r1, #0
+	outchar r0, r1
+	loadn r0, #' '
+	loadn r1, #1
+	outchar r0, r1
+	loadn r0, #' '
+	loadn r1, #2
+	outchar r0, r1
+	loadn r0, #' '
+	loadn r1, #3
+	outchar r0, r1
+	loadn r0, #' '
+	loadn r1, #4
+	outchar r0, r1
+	loadn r0, #' '
+	loadn r1, #5
+	outchar r0, r1
+	loadn r0, #' '
+	loadn r1, #6
+	outchar r0, r1
+	loadn r0, #' '
+	loadn r1, #7
+	outchar r0, r1
+	loadn r0, #' '
+	loadn r1, #8
+	outchar r0, r1
+	loadn r0, #' '
+	loadn r1, #9
+	outchar r0, r1
+	loadn r0, #' '
+	loadn r1, #10
+	outchar r0, r1
+	loadn r0, #' '
+	loadn r1, #11
+	outchar r0, r1
+	loadn r0, #' '
+	loadn r1, #12
+	outchar r0, r1
+	loadn r0, #' '
+	loadn r1, #13
+	outchar r0, r1
+	loadn r0, #' '
+	loadn r1, #14
+	outchar r0, r1
+	loadn r0, #' '
+	loadn r1, #15
+	outchar r0, r1
+	loadn r0, #' '
+	loadn r1, #16
+	outchar r0, r1
+	loadn r0, #' '
+	loadn r1, #17
+	outchar r0, r1
+	loadn r0, #' '
+	loadn r1, #18
+	outchar r0, r1
+	loadn r0, #' '
+	loadn r1, #19
+	outchar r0, r1
+
+	loadn r0, #'A'
+	loadn r1, #220
+	outchar r0, r1
+	
+	loadn r0, #'B'
+	loadn r1, #221
+	outchar r0, r1
+	
+	loadn r0, #'C'
+	loadn r1, #222
+	outchar r0, r1
+	
+	loadn r0, #'D'
+	loadn r1, #223
+	outchar r0, r1
+
+	
+	halt
